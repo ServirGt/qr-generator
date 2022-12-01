11c06c44f98fa55a314bedc959ce0583a9d5caf4
LucaBia <gian.luca.99@hotmail.com>
2022-12-01 15:25:44 -0600

    funcion generar link codigo qr

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 
+function qr(){
+  let vc2 = "BEGIN:VCARD"
+  let vc3 = "N:Heinemann;Rodolfodd"
+  let vc4 = "FN:Rodolfo Heinemann"
+  let vc5 = "ORG:CENDIS"
+  let vc6 = "TITLE:Director General"
+  let vc7 = "ADR:;;2a Calle 9-15;Zona 10;CA;Edificio Mira Of. 101;Guatemala, Guatemala"
+  let vc8 = "TEL;WORK;VOICE:+502 23795300"
+  let vc9 = "TEL;CELL:+502 54035000"
+  let vc10 = "TEL;MAIN:+502 23795303"
+  let vc11 = "EMAIL;WORK;INTERNET:rodolfoh@centrodistribuidor.com"
+  let vc12 = "URL:https://cendis.com.gt"
+  let vc13 = "END:VCARD"
+  let vc14 = "dark=001E61"
+
+  let vc = vc2 + "\n" + vc3 + "\n" + vc4 + "\n" + vc5 + "\n" + vc6 + "\n" + vc7 + "\n" + vc8 + "\n" + vc9 + "\n" + vc10 + "\n" + vc11 + "\n" + vc12 + "\n" + vc13
+
+  console.log(vc)
+
+  console.log(encodeURIComponent(vc))
+  return "https://quickchart.io/qr?text=" + encodeURIComponent(vc) + "&" + vc14
+
+}
+
 function App() {
+  let qrCode = qr()
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. {qrCode}
         </p>
         <a
           className="App-link"
